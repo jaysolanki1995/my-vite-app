3def720c092097b9fbf97591060b95846c327e69
jay <jaysolanki.1995@gmail.com>
2025-05-25 10:12:02 +0530

    chnage new

--- src/App.tsx
+++ src/App.tsx
@@ -20,30 +20,6 @@ function App() {
 
   return (
     <>
-      {/* <div className='hidden'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1 className='hidden'>Vite + React</h1>
-      <div className="card hidden">
-        <button onClick={()=>dispatch(fetchProducts({id:1}))}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <button className='hidden' onClick={()=>dispatch(fetchProducts({id:undefined}))}>yes I am</button>
-      <p className="read-the-docs hidden">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button className='hidden' disabled={disabled} onClick={()=>dispatch(createNewUser({ username: 'john_doe', email: 'john@example.com', password: 'pass123' }))}>fetch based on id</button>
-       */}
       <PockerPointTracker />
     </>
   )
