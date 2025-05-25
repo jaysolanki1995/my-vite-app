3a0da1cde6ba364519c6493cf3aa119a27de73c6
jay <jaysolanki.1995@gmail.com>
2025-05-25 10:06:51 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import { useDispatch} from 'react-redux';
-// import { increment, decrement } from './Redux/incrementSlice';
-import useApiState from './customHooks/useApiState';
-import { createNewUser, fetchProducts } from './thunks/productThunk';
-import { AppDispatch } from './store';
+// import { useDispatch} from 'react-redux';
+// // import { increment, decrement } from './Redux/incrementSlice';
+// import useApiState from './customHooks/useApiState';
+// import { createNewUser, fetchProducts } from './thunks/productThunk';
+// import { AppDispatch } from './store';
 // import { resetData } from './Redux/productSlice';
 import PockerPointTracker from './compnents/PockerPoint';
 
 
 function App() {
-  const dispatch = useDispatch<AppDispatch>(); // Use the typed dispatch
+  // const dispatch = useDispatch<AppDispatch>(); // Use the typed dispatch
  
-  const count =  useApiState("counter","count");
-  const disabled =  useApiState("product","loading");
+  // const count =  useApiState("counter","count");
+  // const disabled =  useApiState("product","loading");
 
 
 
   return (
     <>
-      <div className='hidden'>
+      {/* <div className='hidden'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -43,7 +43,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <button className='hidden' disabled={disabled} onClick={()=>dispatch(createNewUser({ username: 'john_doe', email: 'john@example.com', password: 'pass123' }))}>fetch based on id</button>
-      
+       */}
       <PockerPointTracker />
     </>
   )
